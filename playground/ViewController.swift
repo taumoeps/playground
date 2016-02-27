a897b8d299e5ee9246ca6b33478e08802aa5e87a
S W Taumoepeau <taumoeps@ss-macbook-pro.station>
2016-02-27 22:18:51 +1300

    Initial Commit

---
+++ playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by S W Taumoepeau on 27/02/16.
+//  Copyright © 2016 S W Taumoepeau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
