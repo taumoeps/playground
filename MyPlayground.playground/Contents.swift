9f178cc8560e6e5c6f68425093771a21d22c5064
taumoeps <steven.taumoepeau@gmail.com>
2016-02-27 23:09:14 +1300

    test

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print(str)
+print(str)
+str = str + str
+
+let possibleTipsInferred = [0.15, 0.18, 0.20]
+let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+let param = ["user":["id":2,"firstname":"steven","lastname":"taumoepeau"]]
+let userid = param["user"]!["id"]
+let userfirstname = param["user"]!["firstname"]
+let userlastname = param["user"]!["lastname"]
+
+
+
